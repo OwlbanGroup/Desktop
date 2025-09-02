76c05b2be61da4d29e1a5943bd3713febdec8927
OwlbanGroup <142179795+OwlbanGroup@users.noreply.github.com>
2025-09-02 15:39:33 -0400

    update

--- start_backend.ps1
+++ start_backend.ps1
@@ -1,3 +1,3 @@
-# PowerShell script to start the Flask backend server in a new terminal window
+te# PowerShell script to start the Flask backend server in a new terminal window
 
 Start-Process powershell -ArgumentList '-NoExit', '-Command', 'cd .\backend; python app_server.py'
